1a56253d5aaddbf8079e39d0ca552fcda7600e62
Katelyn Clemow <kgclemow@iu.edu>
2020-02-18 16:14:55 -0500

    Completes particle work

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+
 onready var Camera = $Camera
+onready var add_trauma
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("shake"):
+		add_trauma(1)
+	
+
